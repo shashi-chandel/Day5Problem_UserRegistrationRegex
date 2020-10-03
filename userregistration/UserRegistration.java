98a7b6f6a3c6cb9c9d014bd826f4287fd790e696
Your Name <you@example.com>
2020-10-03 05:59:41 +0530

    [shashi-chandel] add . User Registion Form Started

---
+++ userregistration/UserRegistration.java
@@ -0,0 +1,11 @@
+package com.capgemini.userregistration;
+
+import java.util.*;
+
+public class UserRegistration {
+		public static void main(String[] args) {
+				Scanner sc = new Scanner(System.in);
+				System.out.println("Welcome to User Registration!!");
+				
+}
+}
