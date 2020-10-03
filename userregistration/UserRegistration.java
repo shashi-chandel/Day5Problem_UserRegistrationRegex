57612c4f5e661a2b3cf134d5cc5aaf9101b234fc
Your Name <you@example.com>
2020-10-03 06:43:19 +0530

    [shashi-chandel] add . EmailId Validation is checked

--- userregistration/UserRegistration.java
+++ userregistration/UserRegistration.java
@@ -7,27 +7,37 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String first, last;
-		System.out.println("Enter your first name: ");
-		first = sc.nextLine();
 		String namePattern = "[A-Z]{1}[a-zA-Z]{2,}";
 		do {
 			System.out.println("Enter your first name: ");
 			first = sc.nextLine();
+			if (Pattern.matches(namePattern, first))
+				System.out.println("Valid first name");
+			else
+				System.out.println("Invalid first name!!");
 		} while (!Pattern.matches(namePattern, first));
 
-		System.out.println("\nEnter your last name: ");
+		do {
+			System.out.println("\nEnter your last name: ");
 		last = sc.nextLine();
 		if (Pattern.matches(namePattern, last)) {
 			System.out.println("Valid last name");
 		} else {
 			System.out.println("Invalid last name!!");
-			do {
-				System.out.println("Enter your last name: ");
-				last = sc.nextLine();
-			} while (!Pattern.matches(namePattern, last));
-			System.out.println("Valid  last name");
+			}
+		}while (!Pattern.matches(namePattern, last));
 			System.out.println("\nUser name is " + first + " " + last);
-			sc.close();
-		}
+			
+		String email;
+		String emailPattern = "^[a-z]+[+-.]{0,1}[a-z0-9]+@[a-z0-9]+[.]{1}[a-z]{2,}([.]{0,1}[a-z]{2,}){0,1}([,]){0,1}$";
+		do {
+			System.out.println("\nEnter your email id: ");
+			email = sc.nextLine();
+			if(Pattern.matches(emailPattern, email))
+				System.out.println("Valid email Id!");
+			else
+				System.out.println("Invalid email!!");
+		} while (!Pattern.matches(emailPattern, email));
+		System.out.println("\nUser email id is " + email);
 	}
 }
