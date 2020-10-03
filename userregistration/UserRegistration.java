fd60835cf920666e6e53dc66768ea60bdd15ec7a
Your Name <you@example.com>
2020-10-03 07:05:31 +0530

    [shashi-chandel] add . Mobile Number Validation is checked

--- userregistration/UserRegistration.java
+++ userregistration/UserRegistration.java
@@ -39,5 +39,29 @@ public class UserRegistration {
 				System.out.println("Invalid email!!");
 		} while (!Pattern.matches(emailPattern, email));
 		System.out.println("\nUser email id is " + email);
-	}
+		
+		String mobilePattern = "[1-9]{2}[' ']{1}[789]{1}[0-9]{9}";
+		String mobileNumber;
+			do {
+				System.out.println("\nEnter mobile number with country code: ");
+				mobileNumber = sc.nextLine();
+				if (Pattern.matches(mobilePattern, mobileNumber)) 
+					System.out.println("Valid mobile number");
+				else
+					System.out.println("Invalid Mobile Number!!");
+			} while (!Pattern.matches(mobilePattern, mobileNumber));
+			System.out.println("\nUser mobile number is " + mobileNumber);
+			
+			String passPattern = "[a-zA-Z0-9@#$%^*.,/]{8,}";
+			String password;
+			do {
+				System.out.println("\nEnter your Paasword");
+				password = sc.nextLine();
+				if (Pattern.matches(passPattern, password)) 
+				System.out.println("Valid password");
+				else 
+					System.out.println("Invalid password!!");
+				} while (!Pattern.matches(passPattern, password));
+				System.out.println("\nUser password is " + password);
+		}
 }
