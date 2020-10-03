3d901dbe496891cf91c3d3bde58bfc4f82e68f8d
Your Name <you@example.com>
2020-10-03 06:04:43 +0530

    [shashi-chandel] add . First Name Validation is checked

--- userregistration/UserRegistration.java
+++ userregistration/UserRegistration.java
@@ -1,11 +1,17 @@
 package com.capgemini.userregistration;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class UserRegistration {
 		public static void main(String[] args) {
 				Scanner sc = new Scanner(System.in);
-				System.out.println("Welcome to User Registration!!");
-				
-}
+				System.out.println("Enter your First Name:");
+				String first = sc.nextLine();
+				String namePattern = "[A-Z]{1}[a-zA-Z]{2,}";
+				if(Pattern.matches(namePattern, first))
+	        	     System.out.println("Valid first name");
+	               else 
+	        	     System.out.println("Invalid entry!!");
+				}
 }
